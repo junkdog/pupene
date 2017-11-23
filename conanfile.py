bba627c0c3529b041bafe3a92a4275f0029cdad6
Adrian Papari <a@onedaybeard.net>
2017-11-23 11:30:44 +0100

    conan package with test_package

---
+++ conanfile.py
@@ -0,0 +1,76 @@
+from conans import ConanFile, CMake, tools
+
+class PupeneConan(ConanFile):
+    name = "pupene"
+    version = "0.1.4"
+    license = "MIT"
+    url = "https://github.com/junkdog/pupene"
+    description = "A non-invasive, multi-format serialization library for C++17"
+    settings = "os", "compiler", "build_type", "arch"
+    # options = {"shared": [True, False]}
+    # default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "*"
+    requires = "jsonformoderncpp/2.1.1@vthiery/stable"
+    build_requires = "Boost/1.64.0@conan/stable", \
+                     "glm/0.9.8.5@g-truc/stable", \
+                     "gtest/1.8.0@lasote/stable"
+
+    default_options = "Boost:shared=False", \
+                      "Boost:without_atomic=True", \
+                      "Boost:without_chrono=True", \
+                      "Boost:without_container=True", \
+                      "Boost:without_context=True", \
+                      "Boost:without_coroutine=True", \
+                      "Boost:without_coroutine2=True", \
+                      "Boost:without_date_time=True", \
+                      "Boost:without_exception=True", \
+                      "Boost:without_fiber=True", \
+                      "Boost:without_filesystem=True", \
+                      "Boost:without_graph=True", \
+                      "Boost:without_graph_parallel=True", \
+                      "Boost:without_iostreams=False", \
+                      "Boost:without_locale=True", \
+                      "Boost:without_log=True", \
+                      "Boost:without_math=True", \
+                      "Boost:without_metaparse=True", \
+                      "Boost:without_mpi=True", \
+                      "Boost:without_program_options=True", \
+                      "Boost:without_random=True", \
+                      "Boost:without_regex=True", \
+                      "Boost:without_serialization=True", \
+                      "Boost:without_signals=True", \
+                      "Boost:without_system=True", \
+                      "Boost:without_test=True", \
+                      "Boost:without_thread=True", \
+                      "Boost:without_timer=True", \
+                      "Boost:without_type_erasure=True", \
+                      "Boost:without_wave=True", \
+                      "gtest:shared=False", \
+                      "gtest:no_gmock=True"
+
+    def config(self):
+        self.settings.compiler.libcxx = "libstdc++11"
+
+    # def source(self):
+    #     self.run("git clone https://github.com/junkdog/pupene.git")
+    #     self.run("cd pupene && git checkout conenize")
+
+    def build(self):
+        cmake = CMake(self)
+        # cmake.configure(source_dir="%s/pupene" % self.source_folder, build_dir="./")
+        cmake.configure(source_dir=self.source_folder)
+        cmake.build(target="install")
+
+        # Explicit way:
+        # args = ['-DCMAKE_INSTALL_PREFIX="%s"' % self.package_folder]
+        # self.run('cmake %s/pupene %s %s' % (self.source_folder, cmake.command_line, ' '.join(args)))
+        # self.run('cmake %s/pupene %s' % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . --target install %s" % cmake.build_config)
+
+    def package(self):
+        print("hello")
+
+    def package_info(self):
+        self.cpp_info.libs = ["pupene"]
+
---
+++ test_package/example.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include "pupdefs.h"
+#include <pupene/json.h>
+#include <pupene/binary.h>
+#include <fstream>
+
+const std::string raw_json = R"(
+{
+  "path": "font/primary.fnt",
+  "inner": {
+    "r": 1,
+    "g": 1,
+    "b": 0.5,
+    "a": 1
+  },
+  "outer": {
+    "r": 0.8,
+    "g": 0.1,
+    "b": 0.5,
+    "a": 1
+  }
+}
+)";
+
+int main() {
+    // load from json string
+    auto fr = pupene::from_json<FontReference>(raw_json);
+
+    // save binary representation to file
+    std::ofstream out{"font_ref.bin", std::ofstream::out};
+    pupene::to_binary(fr, out);
+    out.close();
+
+    // load from binary data - debug print pup-ing
+    std::ifstream in{"font_ref.bin", std::ofstream::in};
+    auto ignored_fr = pupene::from_binary<FontReference>(in, true);
+    in.close();
+
+    return 0;
+}
